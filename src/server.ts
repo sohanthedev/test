cd89f6d15cda2d81a0af2fbbbca8ef15317b03fa
sohanthedev <sohandev22@gmail.com>
2023-06-08 15:40:04 +0600

    test

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from './app'
+import config from "./config";
+async function main() {
+ 
+ try {
+   await mongoose.connect(config.database_url as string);
+   console.log('database connect')
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+  })
+ 
+ }
+ catch (err) {
+  console.log('connect failed')
+ }
+}
+main()
